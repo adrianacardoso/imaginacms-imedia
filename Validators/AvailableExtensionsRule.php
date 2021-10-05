725a3e952b91b8028451d94b88574628e8a54c68
Juan Escalante <jcec007@gmail.com>
2021-10-05 18:03:28 -0500

    +Update: make customizable AvailableExtensionsRule +Update: FileHelper.php | change getExtension function to public

--- Validators/AvailableExtensionsRule.php
+++ Validators/AvailableExtensionsRule.php
@@ -7,7 +7,18 @@ use Illuminate\Contracts\Validation\Rule;
 class AvailableExtensionsRule implements Rule
 {
   
-
+  private $extensionsAvailable;
+  private $message;
+  /**
+   * Create a new rule instance.
+   *
+   * @return void
+   */
+  public function __construct($extensionsAvailable = null, $message = null)
+  {
+    $this->extensionsAvailable = $extensionsAvailable ?? mediaExtensionsAvailable();
+    $this->message = $message ?? trans("media::messages.invalidExtensions");
+  }
     /**
      * Determine if the validation rule passes.
      *
@@ -17,7 +28,7 @@ class AvailableExtensionsRule implements Rule
      */
     public function passes($attribute, $value)
     {
-      $intersection = array_diff(is_array($value) ? $value : [$value],mediaExtensionsAvailable());
+      $intersection = array_diff(is_array($value) ? $value : [$value], $this->extensionsAvailable);
  
       if(!empty($intersection))  return false;
       else return true;
@@ -30,6 +41,6 @@ class AvailableExtensionsRule implements Rule
      */
     public function message()
     {
-        return trans("media::messages.invalidExtensions");
+        return $this->message;
     }
 }
