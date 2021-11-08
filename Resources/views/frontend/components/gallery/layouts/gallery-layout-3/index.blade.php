4299d2056264fc202e72a4c76d916b7d514bb4d2
Cristian Marin <marin101998@gmail.com>
2021-11-08 11:31:40 -0500

    +Add | Layout 3 component gallery

--- Resources/views/frontend/components/gallery/layouts/gallery-layout-3/index.blade.php
+++ Resources/views/frontend/components/gallery/layouts/gallery-layout-3/index.blade.php
@@ -1,10 +1,18 @@
 @if(count($gallery) > 0)
-  <div id="{{$id}}Carousel" class="owl-carousel owl-theme">
+  <main>
     @foreach($gallery as $item)
       <div class="item">
         <x-media::single-image :isMedia="true" :mediaFiles="$item" :dataFancybox="$dataFancybox"/>
       </div>
     @endforeach
-  </div>
-  @include("media::frontend.components.gallery.script")
+  </main>
 @endif
+
+<style>
+    main {
+        display: grid;
+        grid-gap: 1rem;
+        grid-template-columns: 200px 200px 200px;
+        grid-template-rows: 150px 150px;
+    }
+</style>
