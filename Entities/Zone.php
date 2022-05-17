eb0d6f593341f7f5ede360d53b5330c6bb46b659
msolano <msolano@imaginacolombia.com>
2022-05-17 18:02:52 -0500

    +Add: MediaRelations trait to entity Zone

--- Entities/Zone.php
+++ Entities/Zone.php
@@ -4,20 +4,21 @@ namespace Modules\Media\Entities;
 
 use Astrotomic\Translatable\Translatable;
 use Modules\Core\Icrud\Entities\CrudModel;
+use Modules\Media\Support\Traits\MediaRelation;
 
 class Zone extends CrudModel
 {
-  
-  
+  use MediaRelation;
+
   protected $table = 'media__zones';
   public $transformer = 'Modules\Media\Transformers\ZoneTransformer';
   public $requestValidation = [
     'create' => 'Modules\Media\Http\Requests\CreateZoneRequest',
     'update' => 'Modules\Media\Http\Requests\UpdateZoneRequest',
   ];
-  
+
   protected $fillable = [
-    
+
     "parent_entity_type",
     "parent_entity_id",
     "entity_type",
@@ -25,7 +26,7 @@ class Zone extends CrudModel
     "name",
     "options",
   ];
-  
+
   protected $casts = [
     'options' => 'array'
   ];
