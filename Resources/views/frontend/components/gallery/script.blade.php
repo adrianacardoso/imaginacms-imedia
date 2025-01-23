8816bb913b7574a9b2b121dc9f9dde424db58d30
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2025-01-23 11:48:50 -0500

    Wrap blade gallery component with liveware

--- Resources/views/frontend/components/gallery/script.blade.php
+++ Resources/views/frontend/components/gallery/script.blade.php
@@ -1,19 +1,31 @@
 <script>
-  $(document).ready(function () {
-    var owl = $('#{{$id}}Carousel');
-    owl.owlCarousel({
-      stagePadding: {!!$stagePadding!!},
-      autoplayTimeout:{!!$autoplayTimeout!!},
-      loop: {!! $loopGallery ? 'true' : 'false' !!},
-      lazyLoad: true,
-      margin: {!! $margin !!},
-      {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
-      dots: {!! $dots ? 'true' : 'false' !!},
-      responsiveClass: {!! $responsiveClass ? 'true' : 'false' !!},
-      autoplay: {!! $autoplay ? 'true' : 'false' !!},
-      autoplayHoverPause: {!! $autoplayHoverPause ? 'true' : 'false' !!},
-      nav: {!! $nav ? 'true' : 'false' !!},
-      responsive: {!!$responsive!!}
+    function initializeOwlCarousel() {
+        var owl = $('#{{$id}}Carousel');
+        owl.owlCarousel({
+            stagePadding: {!!$stagePadding!!},
+            autoplayTimeout: {!!$autoplayTimeout!!},
+            loop: {!! $loopGallery ? 'true' : 'false' !!},
+            lazyLoad: true,
+            margin: {!! $margin !!},
+            {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
+            dots: {!! $dots ? 'true' : 'false' !!},
+            responsiveClass: {!! $responsiveClass ? 'true' : 'false' !!},
+            autoplay: {!! $autoplay ? 'true' : 'false' !!},
+            autoplayHoverPause: {!! $autoplayHoverPause ? 'true' : 'false' !!},
+            nav: {!! $nav ? 'true' : 'false' !!},
+            responsive: {!!$responsive!!}
+        });
+    }
+
+    $(document).ready(function () {
+        initializeOwlCarousel();
     });
-  });
+
+    document.addEventListener('livewire:load', function () {
+        Livewire.on('refreshOwlCarousel'.{{$componentId}}, () => {
+            var owl = $('#{{$id}}Carousel');
+            owl.owlCarousel('destroy');
+            initializeOwlCarousel();
+        });
+    });
 </script>
