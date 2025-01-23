d85a0315efbaeed1fe1afff339906515457088df
Cristian Marin <marin101998@gmail.com>
2025-01-23 11:14:25 -0500

    + fix | v10 23-01-25 upgrade scripts gallery

--- Resources/views/frontend/components/gallery/script.blade.php
+++ Resources/views/frontend/components/gallery/script.blade.php
@@ -1,5 +1,5 @@
 <script>
-    $(document).ready(function () {
+    function initializeOwlCarousel() {
         var owl = $('#{{$id}}Carousel');
         owl.owlCarousel({
             stagePadding: {!!$stagePadding!!},
@@ -15,26 +15,17 @@
             nav: {!! $nav ? 'true' : 'false' !!},
             responsive: {!!$responsive!!}
         });
+    }
+
+    $(document).ready(function () {
+        initializeOwlCarousel();
     });
 
     document.addEventListener('livewire:load', function () {
         Livewire.on('refreshOwlCarousel'.{{$componentId}}, () => {
             var owl = $('#{{$id}}Carousel');
             owl.owlCarousel('destroy');
-            owl.owlCarousel({
-                stagePadding: {!!$stagePadding!!},
-                autoplayTimeout: {!!$autoplayTimeout!!},
-                loop: {!! $loopGallery ? 'true' : 'false' !!},
-                lazyLoad: true,
-                margin: {!! $margin !!},
-                {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
-                dots: {!! $dots ? 'true' : 'false' !!},
-                responsiveClass: {!! $responsiveClass ? 'true' : 'false' !!},
-                autoplay: {!! $autoplay ? 'true' : 'false' !!},
-                autoplayHoverPause: {!! $autoplayHoverPause ? 'true' : 'false' !!},
-                nav: {!! $nav ? 'true' : 'false' !!},
-                responsive: {!!$responsive!!}
-            });
+            initializeOwlCarousel();
         });
     });
 </script>
