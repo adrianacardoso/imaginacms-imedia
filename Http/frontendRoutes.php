eb50984d0c446f0abbf3a4f487cacd94fcc02465
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2025-01-29 17:03:43 -0500

    + fix | v10 29-01-25 private images (#48)

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -4,12 +4,10 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Storage;
 
 /** @var Router $router */
-Route::prefix('/storage')->group(function (Router $router) {
-    $router->get('/assets/media/{criteria}', [
-        'as' => 'public.media.media.show',
-        'uses' => 'Frontend\MediaController@show',
-    ]);
-});
+$router->get('/assets/media/{route?}/{criteria}', [
+  'as' => 'public.media.media.show',
+  'uses' => 'Frontend\MediaController@show',
+])->where('route', '.*');
 /*
 $router->get('storage/assets/media/{path}',[
     'as' => 'public.media.media.show',
